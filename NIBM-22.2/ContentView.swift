c6a452cab50850044e6208d1a3fae356913b43fe
myrifai <yasirrifai30@gmail>
2023-12-14 18:01:09 +0530

    Initial Commit

---
+++ NIBM-22.2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NIBM-22.2
+//
+//  Created by Rifai, Yasir (CHICO-C) on 2023-12-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NIBM-22.2/NIBM_22_2App.swift
@@ -0,0 +1,17 @@
+//
+//  NIBM_22_2App.swift
+//  NIBM-22.2
+//
+//  Created by Rifai, Yasir (CHICO-C) on 2023-12-14.
+//
+
+import SwiftUI
+
+@main
+struct NIBM_22_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
